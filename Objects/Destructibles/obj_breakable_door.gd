b41e1c006a3d49ca4e2eace707ccd0877c32ce7c
NightHackreal <155101537+NightHackreal@users.noreply.github.com>
2024-01-06 10:26:51 -0800

    more crap + a very WIP background system rework

---
+++ Objects/Destructibles/obj_breakable_door.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+func _ready():
+	if (global.saveroom.has(global.targetRoom + name)):
+		queue_free()
+	
+func destroy():
+	utils.instance_create_level(global_position.x + 16, global_position.y + 16, "res://Objects/Visuals/obj_debris.tscn")
+	utils.instance_create_level(global_position.x + 16, global_position.y + 16, "res://Objects/Visuals/obj_debris.tscn")
+	utils.instance_create_level(global_position.x + 16, global_position.y + 16, "res://Objects/Visuals/obj_debris.tscn")
+	utils.instance_create_level(global_position.x + 16, global_position.y + 16, "res://Objects/Visuals/obj_debris.tscn")
+	if (utils.soundplaying("BreakBlock1") || utils.soundplaying("BreakBlock2")):
+		utils.stopsound("BreakBlock1")
+		utils.stopsound("BreakBlock2")
+	var rng = utils.randi_range(1,2)
+	if (rng == 1):
+		utils.playsound("BreakBlock1")
+	elif (rng == 2):
+		utils.playsound("BreakBlock2")
+	global.saveroom.append(global.targetRoom + name)
+	var pizzacoin = utils.randi_range(1, 100)
+	if (pizzacoin >= 95):
+		var pizzacoininst = utils.instance_create_level(global_position.x, global_position.y, "res://Objects/Collectibles/obj_pizzacoin.tscn")
+		pizzacoininst.velocity.x = 2
+	queue_free()
